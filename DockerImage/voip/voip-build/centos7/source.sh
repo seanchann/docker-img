d3591a2dea0ebd306e9e8875a5a8078f856c90c7
xqzhou <zhouxiaoqiang.mstech@gmail.com>
2015-06-17 09:53:09 +0800

    delete voip, restore it with voip-server project

--- DockerImage/voip/voip-build/centos7/source.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp -rf /home/smit/project/voip/opensips/voip-server-publish .
-rm -rf ./voip-server
-mv -f ./voip-server-publish ./voip-server
-tar -czf voip-server.tar.gz ./voip-server
